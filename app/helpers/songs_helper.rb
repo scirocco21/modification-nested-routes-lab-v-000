27742b32c135ecdc137ea43446cd82a6de9b250a
Seb Gertz <sebgertz@gmail.com>
2018-03-15 03:31:06 +0000

    moved

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,10 +1,3 @@
 module SongsHelper
-  def artist_select(song, artist)
-    if song.artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    else
-      @artist.name
-      hidden_field_tag "song[artist_id]", song.artist_id
-    end
-  end
+  
 end
